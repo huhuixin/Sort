d9914d3b0fb12caf2fac9229f502edc6f75c96e6
huhuixin <18611551449@163.com>
2017-08-12 23:23:51 +0800

    添加Log模块

--- Sort/js/log.js
+++ Sort/js/log.js
@@ -1,9 +1,9 @@
-const base_url = 'http://localhost:8080';
+var base_url = 'http://www.huhuixin.com:10000';
 $(document).ready(function(){
     function accessLog()
     {
         $.ajax({
-            method: "POST",
+            method: "GET",
             async: false,
             url: base_url + "/log/v1/access",
             data:{
@@ -11,17 +11,21 @@ $(document).ready(function(){
                 page_name : $("input[name=page_name]").val(),
                 share_msg : $("input[name=share_msg]").val(),
                 other_msg : $("input[name=other_msg]").val(),
+                hardware : navigator.userAgent,
+                address : remote_ip_info['province'] + remote_ip_info['city']
             },
             dataType: "jsonp",
-            //jsonp: true,
+            //jsonp: callback,
             jsonpCallback:"log",
             success: function(data){
-                alert(data.name);
+                console.log(data);
             },
             error: function(){
-                alert('fail');
+                console.log('log_fail');
             }
         });
     }
     accessLog();
 });
+
+
